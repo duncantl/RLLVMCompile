f8146b011422c4290c1110dfa61755e667d295e9
Duncan Temple Lang <duncan@r-project.org>
2011-02-06 13:39:41 -0800

    results for the Sum2 counting program. See eg.R

--- eg.R
+++ eg.R
@@ -13,3 +13,17 @@ run(a$fun, 1000, .ee = ee)
 
 r = system.time(replicate(100, Sum1(1000)))
 ll = system.time(replicate(100, run(a$fun, 1000, .ee = ee)))
+
+B = 10
+n = c(100, 1e3, 1e4, 1e5, 1e6, 1e7)
+rr = sapply(n,
+             function(n) 
+               system.time(replicate(B, Sum2(n))))
+
+ll = sapply(n,
+             function(n) 
+               system.time(replicate(B, run(a$fun, n, .ee = ee))))
+
+plot(n, rr[3,]/ll[3,], type = 'l')
+rr[3,]/ll[3,]
+# [1]   0.0000   1.0000   9.4000  83.0000 317.0625 462.0935
