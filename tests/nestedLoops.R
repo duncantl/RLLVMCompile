4bac9e963b9fe4d24d89b3984b173d99e6a809ca
Duncan Temple Lang <duncan@r-project.org>
2013-05-05 13:06:28 -0700

    local changes

--- tests/nestedLoops.R
+++ tests/nestedLoops.R
@@ -16,9 +16,10 @@ library(compiler)
 fcc = cmpfun(f)
 
 n = 1e4
+ee = ExecutionEngine(as(fc, "Module"))
 tm.1e4 = rbind(r = system.time(f(n)),
                byte = system.time(fcc(n)),
-               ll = system.time(.llvm(fc, n)))[, 1:3]
+               ll = system.time(.llvm(fc, n, .ee = ee)))[, 1:3]
 
 
 apply(tm.1e4, 2, function(x) x/min(x))
@@ -27,7 +28,11 @@ apply(tm.1e4, 2, function(x) x/min(x))
 #r    244.20388      Inf 244.97087
 #byte  60.83495      Inf  61.16505
 #ll     1.00000      NaN   1.00000
-
+#Linux Apr 28, 2013
+#     user.self sys.self   elapsed
+#r     283.6190      NaN 283.66667
+#byte   61.2619      Inf  61.34921
+#ll      1.0000      NaN   1.00000
 
 g = function(n1, n2) {
    ctr = 0L
