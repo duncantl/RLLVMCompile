d72d4b2c64b739b46b5fcf8b796808a846558be5
Duncan Temple Lang <duncan@r-project.org>
2013-07-15 07:19:42 -0700

    compiling a GPU kernel basics working

---
+++ explorations/gpu.R
@@ -0,0 +1,34 @@
+# Here we are compiling an R function as if it were
+# a kernel for a GPU.  
+#
+#
+#
+
+library(RLLVMCompile)
+
+f = function(x)
+{
+  idx = threadIdx$x # blockIdx$x * blockDim$x + threadIdx$x
+  x[idx] = idx  #XXX need to avoid the 1 based indexing
+#  return()
+}
+
+fun = compileGPUKernel(f, list(x = Int32PtrType))
+
+
+if(require(Rnvvm)) {
+m = as(fun, "Module")
+#setDataLayout(m, RLLVMCompile:::DefaultGPULayout)
+#setGPUKernel(fun, m)
+
+code = as(m, "character")
+code = RLLVMCompile:::fixPTXCodeForNVVM(code)
+ptx = generatePTX(code)
+
+library(RCUDA)
+cu.mod = cuModuleLoadDataEx(ptx)
+
+N = 32
+ans = integer(N)
+out = .gpu(cu.mod$f, ans, gridDim = 1, blockDim = 32)
+}
