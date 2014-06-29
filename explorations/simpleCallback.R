a3b208275b6b3b7f56a350bf5aa2fe00db141474
Duncan Temple Lang <duncan@r-project.org>
2014-06-29 06:04:37 -0700

    add calling the create_call method

--- explorations/simpleCallback.R
+++ explorations/simpleCallback.R
@@ -3,17 +3,18 @@ library(RLLVMCompile)
 cb =
 function()
 {
-  ans = foo() # a = 1, b = 2)
+  ans = foo(1, 2) # a = 1, b = 2)
   printf("answer from R: %d\n", ans)
 }
 
 
-fc = compileFunction(cb, VoidType, list(),
+m = Module()
+fc = compileFunction(cb, VoidType, list(), module = m,
                      .CallableRFunctions = list(foo = list(Int32Type, list())),
                      .ee = TRUE)
 
 
-m = as(fc, "Module")
+#m = as(fc, "Module")
 ee = ExecutionEngine(m)
 .llvm(m$setCall_foo_expression, quote(foo()), .ee = ee)
 
@@ -30,3 +31,5 @@ function(x = 1, a = TRUE)
 .llvm(m$setCall_foo_expression, quote(foo(pi, FALSE)), .ee = ee)
 .llvm(fc, .ee = ee)
 
+m$create_foo_expression
+.llvm(m$create_foo_expression, .ee = ee)
