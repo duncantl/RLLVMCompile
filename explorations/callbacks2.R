c99304f2dba343a04d0c101979c3ac145ece6758
Duncan Temple Lang <duncan@r-project.org>
2014-06-30 11:11:23 -0700

    works, and shows different wrinkles

--- explorations/callbacks2.R
+++ explorations/callbacks2.R
@@ -12,12 +12,15 @@ cb =
 function()
 {
   ctr = 20L
-  ans = foo(bob, x = ctr, 1L) # structure(1:10, class = "Jane", names = letters[1:10]))
+  ans = foo(bob, x = ctr, structure(1:10, class = "Jane", names = letters[1:10]))
 }
 
 
 m = Module()
 ee = ExecutionEngine(m)
+
+# Do this with .ee = NULL and .ee = ee.
+# These two are quite different.
 fc = compileFunction(cb, VoidType, list(), module = m,
                      .CallableRFunctions = list(foo = list(Int32Type, list(Int32Type, DoubleType, Int32Type))),
                      .RGlobalVariables = "bob", .ee = NULL)
