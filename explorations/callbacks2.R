68b254689201dfd31cac44ead2085d5406d409cd
Duncan Temple Lang <duncan@r-project.org>
2014-06-30 10:31:17 -0700

    callbacks with R global variable (bob) working

--- explorations/callbacks2.R
+++ explorations/callbacks2.R
@@ -3,6 +3,8 @@ library(RLLVMCompile)
 foo =
 function(x, y = 1, z = TRUE)
 {
+browser()    
+  print(y)  # this will be the value of bob
    10L
 }
 
@@ -10,15 +12,16 @@ cb =
 function()
 {
   ctr = 20L
-  ans = foo(bob, x = ctr, 1L)
+  ans = foo(bob, x = ctr, 1L) # structure(1:10, class = "Jane", names = letters[1:10]))
 }
 
 
 m = Module()
-fc = compileFunction(cb, VoidType, list(), module = m,
-                     .CallableRFunctions = list(foo = list(Int32Type, list(Int32Type, DoubleType, Int32Type))))
-
 ee = ExecutionEngine(m)
+fc = compileFunction(cb, VoidType, list(), module = m,
+                     .CallableRFunctions = list(foo = list(Int32Type, list(Int32Type, DoubleType, Int32Type))),
+                     .RGlobalVariables = "bob", .ee = NULL)
+
 
 bob = 10
 .llvm(fc, .ee = ee)
