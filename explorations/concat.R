d891cfc8b612fd130fda1e01be5d0935c735aa14
Duncan Temple Lang <duncan@r-project.org>
2014-07-02 19:07:18 -0700

    example of loop concatenation

--- explorations/concat.R
+++ explorations/concat.R
@@ -1,3 +1,25 @@
-x = c()
- for(i in z)
-    x = c(x, f(i))
+e = quote({
+    x = c()
+    for(i in z) 
+       x = c(x, f(i))
+})
+
+f =
+function(z)
+{    
+    x = c()
+    for(i in z) 
+       x = c(x, f(i))
+   x
+}
+
+e1 = parse(text = "    x = c()
+    for(i in z) {
+       x = c(x, f(i))
+    }")
+
+# e has length 3 with e[[1]] == "{"
+# e1 has length 2
+
+
+
