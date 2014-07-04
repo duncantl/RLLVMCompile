508c55037b6026c598e2ce5d458dfb21eb772444
Duncan Temple Lang <duncan@r-project.org>
2014-07-04 16:26:43 -0700

    additional tests for unary operations

--- tests/unary.R
+++ tests/unary.R
@@ -5,4 +5,20 @@ function(x)
   - x
 
 fc = compileFunction(f, Int32Type, Int32Type)
+.llvm(fc, 20)
+
+
+g =
+function()
+  - 9
+gc = compileFunction(g, Int32Type)
+.llvm(gc)
+
+h =
+function()
+  +2
+hc = compileFunction(h, Int32Type)
+.llvm(hc)
+
+
 
