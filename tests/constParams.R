aa656791d51bfa98a28ff5c82aa8d67e0956c4a3
Duncan Temple Lang <duncan@r-project.org>
2014-07-10 19:44:17 -0700

    test for const parameters

--- tests/constParams.R
+++ tests/constParams.R
@@ -15,11 +15,14 @@ fc = compileFunction(f, DoubleType, list(pointerType(DoubleType), Int32Type), mo
 g =
 function(x, n, ans)
 {
+printf("hi\n")    
     for(i in 1:n) 
         ans[i] = 2. * x[i]
 }
 m = Module()
 gc = compileFunction(g, VoidType, list(pointerType(DoubleType), Int32Type, pointerType(DoubleType)), module = m)
+setMetadata(m, "g:ans", list("argStyle", "write-only"))
 
 a = as.numeric(1:10)
+#debug(.llvm)
 ans = .llvm(gc, a, length(a), numeric(length(a)), .all = TRUE)[[3]]
