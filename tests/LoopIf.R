807be0b88ce5f89aabd60875cd2a2964cc7ddfbc
Duncan Temple Lang <duncan@r-project.org>
2015-02-15 17:22:52 -0800

    more code to test

--- tests/LoopIf.R
+++ tests/LoopIf.R
@@ -1,5 +1,9 @@
 library(RLLVMCompile)
 
+# Checks an if statement inside a loop.
+#
+# Need to also test an if statement with a continue/next/break
+
 f =
 function(x, n)
 {
@@ -10,3 +14,9 @@ function(x, n)
   }
   ctr
 }
+
+f = compileFunction(f, Int32Type, list(INTSXPType, Int32Type))
+#ee = ExecutionEngine(f)
+set.seed(13125)
+v = rpois(100, 4)
+stopifnot(.llvm(f, v, length(v)) == sum(v > 2))
