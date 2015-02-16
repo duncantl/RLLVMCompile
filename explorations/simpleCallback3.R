4ea41522ba48a10c31f9038a614f627f148fa937
Duncan Temple Lang <duncan@r-project.org>
2015-02-15 17:12:16 -0800

    comments explaining the example

--- explorations/simpleCallback3.R
+++ explorations/simpleCallback3.R
@@ -1,5 +1,20 @@
 library(RLLVMCompile)
 
+# This is the clean version of the callback examples that requires the user do the least.
+# The others expose setting the callback expressions.
+
+
+# This illustrates how a function we compile into LLVM native code
+# can call back to an R function.
+# Here cb is the function we compile; foo() is the R function
+# that we call back to.
+
+# When we compile cb, we tell the compiler that foo() is an R function
+# and we give it foo()'s signature.
+
+#XXX We can make compileFunction() handle variable arguments to foo() and we would provide the
+# signature for the call with the most parameters, or actually the union of the arguments
+# across the calls.
 
 foo =
 function(x = 1L, a = TRUE)
