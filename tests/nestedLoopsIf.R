8578cc4aa3c41e60e08fe84f0f2979481af73b2a
Duncan Temple Lang <duncan@r-project.org>
2015-02-18 17:38:01 -0800

    activate the if()

--- tests/nestedLoopsIf.R
+++ tests/nestedLoopsIf.R
@@ -4,8 +4,8 @@ f = function(n) {
    ctr = 0L
    for(i in 1:n)
       for(j in 1:i) {
-#         if( i == j)
-           #  printf("diagonal %d\n", i)
+         if( i == j)
+            printf("diagonal %d\n", i)
          ctr = ctr + 1L
       }
    ctr
