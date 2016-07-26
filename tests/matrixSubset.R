13d5a6d63f6fa8d722b66f312645c0ebaa56670f
Duncan Temple Lang <duncan@r-project.org>
2016-07-26 12:43:18 -0700

    tests for matrix subsetting. The code generation is correct. Unfortunately, the mcjit doesn't seem to know how to make the calls with these signatures yet

--- tests/matrixSubset.R
+++ tests/matrixSubset.R
@@ -7,17 +7,37 @@ function(x, i)
 }
 
 m = Module()
-fc = compileFunction(f, StringType, list(MatrixType(StringType), Int32Type), m)
+fc = compileFunction(f, StringType, list(RMatrixType(StringType), Int32Type), m)
+showModule(m)
 .llvm(fc, matrix(letters, 13, 2), 3)  # "p"
 
 m = Module()
-dc = compileFunction(f, DoubleType, list(MatrixType(DoubleType), Int32Type), m)
+dc = compileFunction(f, DoubleType, list(RMatrixType(DoubleType), Int32Type), m)
+showModule(m)
 .llvm(dc, matrix(1:26 + .01, 13, 2), 3)  # "p"
 
 
+f =
+function(x, i)
+{
+  x[i, 5L]
+}
 
 m = Module()
-gc = compileFunction(f, Int32Type, list(MatrixType(Int32Type), Int32Type), m)
+gc = compileFunction(f, Int32Type, list(RMatrixType(Int32Type), Int32Type), m)
+showModule(m)
+
 .llvm(gc, matrix(1:26, 13, 2), 3)  # "p"
+
+
+f =
+function(x, i, j)
+{
+  x[i, j]
+}
+
+m = Module()
+gc = compileFunction(f, Int32Type, list(RMatrixType(Int32Type), Int32Type, Int32Type), m)
+showModule(m)
 
 
--- tests/matrixSubsetCmp.R
+++ tests/matrixSubsetCmp.R
@@ -7,11 +7,12 @@ function(x, i)
 }
 
 m = Module()
-fc = compileFunction(f, Int1Type, list(MatrixType(Int32Type), Int32Type), m)
+fc = compileFunction(f, Int1Type, list(RMatrixType(Int32Type), Int32Type), m)
 .llvm(fc, matrix(1:10, 5, 2)[, 2:1], 1)
 
+# Fails - comparing double to Int32Type
 m = Module()
-gc = compileFunction(f, Int1Type, list(MatrixType(DoubleType), Int32Type), m)
+gc = compileFunction(f, Int1Type, list(RMatrixType(DoubleType), Int32Type), m)
 .llvm(gc, matrix(1:10 + .1, 5, 2), 1)
 
 
